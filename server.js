dd6b509cb21d99e93a6d9855b8ad7bb737388539
Abdul Hakeem Bawah <abdulhakeembawah@gmail.com>
2026-03-22 21:29:10 +0000

    tokenAccess

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+const jwt = require("jsonwebtoken");
+
+app.use(express.json());
+
 const posts = [
   {
     kwame: "i like basketball",
@@ -12,9 +16,32 @@ const posts = [
 
 const PORT = process.env.PORT;
 
-app.get("/posts", (req, res) => {
-  res.status(200).json(posts);
+app.get("/posts", checkToken, (req, res) => {
+  res.json(posts.filter((post) => post.username === req.user.name));
 });
+
+app.post("/login", (req, res) => {
+  //authenticate user
+
+  const username = req.body.username;
+  const user = { name: username };
+
+  const accessToken = jwt.sign(user, process.env.SECRET_TOKEN);
+  res.json({ accessToken: accessToken });
+});
+
+function checkToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.SECRET_TOKEN, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`serving on port:${PORT}`);
